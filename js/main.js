8d64e315ac26be1d64ff1a7ff7314bddac8201cc
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-09-28 00:39:30 -0300

    Finalizando estilo da página

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 // Script Javascript
 
+
 function numRandom(){
     var num1 = parseInt(Math.random() * 10);
     var num2 = parseInt(Math.random() * 10);
   	document.getElementById("num1").innerHTML = num1;
   	document.getElementById("num2").innerHTML = num2;
+  	soma = num1 + num2;
+  	return soma;
 
+}
+
+
+function numEnviar(){
+	alert("Formulário enviado");
+	
 }
 
