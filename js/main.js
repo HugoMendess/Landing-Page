b63e05fe47dae6bce0e5ed5166c1a55bca5e0671
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-09-28 00:21:24 -0300

    Estilizando o calculo e criando a lógica Javascript

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 
 function numRandom(){
     alert("Preencha o campo");
-    var num1 = random(1,100);
-    alert(num1);
+    var num1 = parseInt(Math.random() * 10);
+    var num2 = parseInt(Math.random() * 10);
+  	document.getElementById()
+
 }
+
